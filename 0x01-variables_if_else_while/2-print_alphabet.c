fd7fa56d7b16d8777e5629300118b315c674bd00
Kenneth Ugochukwu <jexaltation@gmail.com>
2023-02-28 09:42:09 -0800

    C variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,26 +10,13 @@
 
 int main(void)
 {
-	int p, q;
-	for (p = 0; p <= 98; p++)
+	char letter;
 	
-	{
-		for (q = p + 1; q <= 99; q++)
-		{
-			putchar((p / 10) + '0');
-			putchar((p % 10) + '0');
-			putchar('\0');		
-			putchar((q / 10) + '0');
-			putchar((q % 10) + '0');
-
-			if (p == 98 && q == 99)
-				continue;
-
-			putchar(',');
-			putchar('\0');
-	;	}
-	}
-
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	
 	putchar('\n');
+	
 	return (0);
 }
+
