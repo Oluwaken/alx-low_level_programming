f08d4e2cafcfb0e032acbb26e93cb74a84a4810c
Kenneth Ugochukwu <jexaltation@gmail.com>
2023-03-01 05:07:06 -0800

    C variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,14 +15,14 @@ int main(void)
 	for (d = '0'; d <= '9'; d++)
 	{
 		putchar((d % 10) + '0');
-		if (d == '9')
+		if(d == '9')
 			continue;
 		
 			putchar(',');
 			putchar(' ');
 		
 	}
-	putchar('\n')
-
-		return (0);
+	putchar('\n');
+	
+	return (0);
 }
